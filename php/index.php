07acdc80ecdbfd566cfe06212a1b1613061b5515
HongShie <wonghongshie0816@e.newera.edu.my>
2024-02-21 11:06:52 +0800

    new file:   php/index.php

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/index.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="icon" href="./img/yolo-logo-zip-file (1)/yolo-website-favicon-color.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Welcome to YOLO
+        </title>
+    </head>
+    <body>
+        fhjk
+    </body>
+</html>
