62ca657f9e4aa4f863f46d0661bf1084a03dc8da
HongShie <wonghongshie0816@e.newera.edu.my>
2024-02-21 11:17:58 +0800

    modified:   php/index.php

--- php/index.php
+++ php/index.php
@@ -6,7 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="icon" href="./img/yolo-logo-zip-file (1)/yolo-website-favicon-color.png">
+        <link rel="icon" href="/img/logo/yvonnes-art-website-favicon-color.png">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>
             Welcome to YOLO
